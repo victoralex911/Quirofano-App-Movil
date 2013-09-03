335e5cedb5cb0f6e04093f6cd31958685f4e0505
eddypre <eddypre@hotmail.com>
2013-09-03 08:10:31 -0500

    Conexión con JDBC, 01

--- Quirofano/src/com/hu/quirofano/MainActivity.java
+++ Quirofano/src/com/hu/quirofano/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main3);
+		sqlThread.start();
 		// Boton de entrar
 		Button boton_Entrar = (Button) findViewById(R.id.enterButton);
 		boton_Entrar.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +41,32 @@ public class MainActivity extends Activity {
 			}
 		});
 	}//Fin de onCreate
+	
+	/******************************* conexion ***********************/
+	
+	Thread sqlThread = new Thread(){
+		public void run(){
+			try{
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://192.168.1.73:3306/android_login", "root", "PonTuPasswd");
+				String stsql = "Select version()";
+				Statement st = conn.createStatement();
+				ResultSet rs = st.executeQuery(stsql);
+				rs.next();
+				System.out.println( rs.getString(1) );
+				System.out.println("Conexion exitosa");
+				conn.close();
+			}//Fin de try
+			catch (SQLException se) {
+				System.out.println("No se puede conectar. Error: " + se.toString());
+			}//Fin de catch
+			catch (ClassNotFoundException e) {
+			//	 System.out.println("No se encuentra la clase. Error: " + e.getMessage());
+			}//Fin de catch2
+		}//Fin de run
+	};
+	/*Error 01, no hay linkeo entre la aplicacion y la BD*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
