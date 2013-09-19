fced91bec5064d2c780be94b52a066728e062f82
eddypre <eddypre@hotmail.com>
2013-09-19 09:59:50 -0500

    Scripts PHP

---
+++ htdocs/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");//Username
+define("DB_PASSWORD", "yourPass");//Pass
+define("DB_DATABASE", "yourDatabase");//Nombre de la base de datos
+?>
