0ebec47f4e8ec9bdf43bc25913608d915c3f6e6d
Victor <victor.alex911@gmail.com>
2013-09-20 12:08:12 -0500

    Modificando archivos :)

--- Quirofano/src/com/hu/quirofano/MainActivity2.java
+++ Quirofano/src/com/hu/quirofano/MainActivity2.java
@@ -8,58 +8,58 @@ import android.view.KeyEvent;
 import android.view.View;
 
 public class MainActivity2 extends BaseActivity{
-	
+
 	private Fragment mContent;
-	
+
 	public MainActivity2(){
 		super(R.string.app_name);
-		
+
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		
+
 		if (savedInstanceState != null)
 			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
 		if (mContent == null)
 			mContent = new MainView();
-		
+
 		setContentView(R.layout.content_frame); //content_frame
 		getSupportFragmentManager()
 		.beginTransaction()
 		.replace(R.id.content_frame, mContent).commit();
-		
+
 		setBehindContentView(R.layout.menu_frame);
 		getSupportFragmentManager()
 		.beginTransaction()
 		.replace(R.id.menu_frame, new RandomList()).commit();
-		
+
 		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 		setSlidingActionBarEnabled(true);
 	}
-	
+
 	@Override
 	public void onSaveInstanceState(Bundle outState){
 		super.onSaveInstanceState(outState);
 		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
 	}
-	
+
 	public void switchContent(Fragment fragment){
 		mContent = fragment;
 		getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
 		getSlidingMenu().showContent();
 	}
-	
-	//Boton de back
-	
-		 @Override
-		 public boolean onKeyDown(int keyCode, KeyEvent event)  {
-		     if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-		         // no hacemos nada.
-		         return true;
-		     }
 
-		     return super.onKeyDown(keyCode, event);
-		 }
+	//Boton de back
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)  {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			// no hacemos nada.
+			return true;
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
 }
