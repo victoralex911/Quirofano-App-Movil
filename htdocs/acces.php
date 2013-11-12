0ce4ecb743bd73a4f5d24eea6ebb63af73dd9a41
eddypre <eddypre@hotmail.com>
2013-11-12 16:51:28 -0600

    Scripts PHP: mostrarAgenda, agendaDelDia, schedule

--- htdocs/acces.php
+++ htdocs/acces.php
@@ -11,15 +11,14 @@ require_once 'funciones_bd.php';
 $db = new funciones_BD();
 
 	if($db->login($usuario,$passw)){
-
+	
 	$resultado[]=array("logstatus"=>"0");
+		// no hubo coincidencias --acceso denegado
 	}else{
 	$resultado[]=array("logstatus"=>"1");
+		// si hubo coincidencias --acceso permitido	
 	}
 
 echo json_encode($resultado);
-
-
-
 
 ?>
---
+++ htdocs/algoritmo.php
@@ -0,0 +1,7 @@
+<?php
+
+$usuario = 'mesostel';
+
+echo sha1($usuario);
+
+?>
