d4d08e392f7541dda31485d89274102b7b1cf8c1
eddypre <eddypre@hotmail.com>
2013-11-12 17:07:00 -0600

    Update config.php

--- htdocs/config.php
+++ htdocs/config.php
@@ -4,7 +4,7 @@
  */
 define("DB_HOST", "localhost");
 define("DB_USER", "root");//cambiar por el nombre de usuario definido en la configuracion de la BD.
-define("DB_PASSWORD", "Prefontaine9");//Modificar por el password elegido
+define("DB_PASSWORD", "Pass");//Modificar por el password elegido
 define("DB_DATABASE", "Quirofano");//Nombre de la base de datos reemplazar si se utilizo otro diferente al mencionado en el tutorial.
 ?>
 
